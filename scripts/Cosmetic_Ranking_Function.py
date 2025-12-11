66ee4e4bdde7715da2756c6b078679fde26d6390
Jay <jihoonpark4337@gmail.com>
2025-12-10 19:53:18 -0600

    I added the notes, and then I fixed the code to have maxiumum rank equal to 8, which is the same as Think Dirty

--- scripts/Cosmetic_Ranking_Function.py
+++ scripts/Cosmetic_Ranking_Function.py
@@ -16,21 +16,26 @@ def update_ingredient_ranks():
     products = cur.fetchall()  #
 
     for product_id, ingredients_list_str in products:
-        total_rank_score = 0.0
-        max_rank = 8 #Assuming the maxiumum rank for an ingredient is 8. Note (I am not sure if Think Dirty uses this scale or they use weighted average)
+        highest_rank = 0 #We start the total score at 0. And then we will track the highest score found.
+        max_rank_limit = 8 #Assuming the maxiumum rank for an ingredient is 8. Note (I am not sure if Think Dirty uses this scale or they use weighted average)
         
         if ingredients_list_str:
             ingredients = [item.strip() for item in ingredients_list_str.split(',')] #The ingredients in the row are comma separated!
             
             for ingredient in ingredients:
                 if ingredient in ingredient_ranks:
-                    total_rank_score += ingredient_ranks[ingredient]
+                    score = ingredient_ranks[ingredient]
 
-        cur.execute("""
-            UPDATE cosmetics 
-            SET Rank = ?
-            WHERE rowid = ?
-        """, (total_rank_score, product_id))
+                    if score > highest_rank:
+                        highest_rank = score 
+
+            final_product_rank = min(highest_rank, max_rank_limit) #Therefore, if the highest rank was more than 8, the final product rank will be 8 as the function is min
+
+            cur.execute("""
+    UPDATE cosmetics 
+    SET Rank = ?
+    WHERE rowid = ?
+""", (final_product_rank, product_id))
 
     conn.commit()
     conn.close()
